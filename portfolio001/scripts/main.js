b3178f0096e02d9c2e27fe52946bcda84b07adeb
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-03-20 20:25:22 -0300

    portfolio att

---
+++ portfolio001/scripts/main.js
@@ -0,0 +1,17 @@
+function js() {
+    let js = document.getElementById('js');
+    
+}
+
+function css() {
+    let css = document.getElementById('css')
+}
+
+function html() {
+    let html = document.getElementById('html')
+    
+}
+
+function leave() {
+
+}
