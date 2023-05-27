aff6e067f5a59a101b2e04a1ee0ce86f0e030b1b
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-05-26 22:39:47 -0300

    bug fix

--- portfolio002/scripts/main.js
+++ portfolio002/scripts/main.js
@@ -6,3 +6,13 @@ function clickMenu() {
     }
     
 }
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'flex'
+        //menu.style.display = 'none'
+    } else {
+        menu.style.display = 'none'
+        //menu.style.display = 'block'
+    }
+}
